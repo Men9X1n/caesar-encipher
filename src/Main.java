f7ce2f407b6820ce28a68b2a9cebeef62828f4da
95478 <954787484@qq.com>
2024-07-11 10:07:53 +0800

    优化了数组定义长度，使空间复杂度变低

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String str = input.nextLine();
-        char[] a = new char[100];
-        for (int i = 0; i < str.length(); i++) {
-            int covert = str.charAt(i) + 3;
+        char[] str1 = str.toCharArray();
+        for (int i = 0; i < str1.length; i++) {
+            int covert = str1[i] + 3;
             switch (BigOrSmall(covert)) {
                 case 0:
                     if (covert > 90) {
-                        a[i] = (char) (covert - 26);
+                        str1[i] = (char) (covert - 26);
                     } else {
-                        a[i] = (char) (covert);
+                        str1[i] = (char) (covert);
                     }
                     break;
                 case 1:
                     if (covert > 122) {
-                        a[i] = (char) (covert - 26);
+                        str1[i] = (char) (covert - 26);
                     } else {
-                        a[i] = (char) covert;
+                        str1[i] = (char) covert;
                     }
                     break;
                 case 2:
-                    a[i] = (char) 32;
+                    str1[i] = (char) 32;
                     break;
             }
         }
-        for (int i = 0; i < str.length(); i++) {
-            System.out.print(a[i]);
+        for (char c : str1) {
+            System.out.print(c);
         }
     }
 
